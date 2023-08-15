e91e677d311a3457f55c5e8d6f3b1b4911a4dd12
VladStroi <vladislavstroy@gmail.com>
2023-08-15 09:19:50 +0300

    history bug fix

--- my-wallet-app/src/components/history-transactions.jsx
+++ my-wallet-app/src/components/history-transactions.jsx
@@ -1,18 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState, useContext } from "react";
 import { Context } from "../context";
 
 import { HistoryAccordion } from "./history";
 import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
 
-const transactions = JSON.parse(localStorage.getItem("transactionHistory"));
+const [...transactions] = JSON.parse(localStorage.getItem("transactionHistory")) || [];
 
 export const HistoryTransactions = () => {
+
   const value = useContext(Context);
   const [showHistory, setShowHistory] = useState(false);
 
   return (
-    <>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
       {value.address && (
         <Button
           onClick={() => {
@@ -25,8 +32,23 @@ export const HistoryTransactions = () => {
           {!showHistory ? "Show history" : "Hide history"}
         </Button>
       )}
-      {showHistory &&
-        transactions.map((transaction) => <HistoryAccordion key={transaction.date} transaction={transaction} />)}
-    </>
+      {showHistory && transactions.length > 0 &&
+        transactions.map((transaction) => (
+          <HistoryAccordion key={transaction.date} transaction={transaction} />
+        ))}
+        {showHistory && transactions.length === 0 && "No transaction history found. Make the first transaction and reload the page."}
+      {value.address && showHistory && transactions.length > 0 && (
+        <Button
+          onClick={() => {
+            localStorage.removeItem("transactionHistory");
+          }}
+          sx={{ margin: "5vh 0" }}
+          variant="outlined"
+          color="inherit"
+        >
+          Clear transaction history
+        </Button>
+      )}
+    </Box>
   );
 };
